6530dc121ad970e8011132187199d028f37a06d6
lucifer-999 <ayush.bajaj3210@gmail.com>
2021-06-28 21:56:21 +0530

    Added SSH Client

---
+++ ssh/ssh_client.py
@@ -0,0 +1,37 @@
+import getpass
+import paramiko
+import sys
+
+
+def ssh_connect (user, ssh_password, host, ssh_port, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    client.connect(host, port=ssh_port, username=user, password=ssh_password)
+
+    _, output, error = client.exec_command(command)
+
+    result = output.readlines() + error.readline()
+
+    if result:
+        print("Output:\n")
+        for line in result:
+            print(line.strip())
+
+
+def main():
+    if len(sys.argv) != 5:
+        print(f"Usage:\t\tpython {sys.argv[0]} <user> <host/ip of server> <port> <command>")
+        print(f"Example:\tpython {sys.argv[0]} root 127.0.0.1 22 whoami")
+        exit
+
+    user = sys.argv[1]
+    password = getpass.getpass()
+    host = sys.argv[2]
+    port = sys.argv[3]
+    cmd = sys.argv[4]
+
+    ssh_connect(user, password, host, port, command)
+
+if __name__ == "__main__":
+    main()
